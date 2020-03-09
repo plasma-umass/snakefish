77c337cc51de9eeb5309d06a04776d44e6ef373a
Wenjun Huang <wenjunhuang@umass.edu>
2020-03-09 02:30:22 -0400

    implement IPC channel

---
+++ src/channel.cpp
@@ -0,0 +1,106 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+#include <cstdio>
+#include <stdexcept>
+
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "channel.h"
+
+namespace snakefish {
+
+std::tuple<sender, receiver, sender, receiver> sync_channel() {
+  int socket_fd[2];
+  if (socketpair(AF_UNIX, SOCK_DGRAM, 0, socket_fd)) {
+    perror("socketpair() failed");
+    throw std::runtime_error("socketpair() failed");
+  }
+
+  if (fcntl(socket_fd[0], F_SETFD, FD_CLOEXEC)) {
+    perror("fcntl(FD_CLOEXEC) failed for sender socket");
+    throw std::runtime_error("fcntl(FD_CLOEXEC) failed for sender socket");
+  }
+  if (fcntl(socket_fd[1], F_SETFD, FD_CLOEXEC)) {
+    perror("fcntl(FD_CLOEXEC) failed for receiver socket");
+    throw std::runtime_error("fcntl(FD_CLOEXEC) failed for receiver socket");
+  }
+
+  return {sender(socket_fd[0]), receiver(socket_fd[0]),
+          sender(socket_fd[1]), receiver(socket_fd[1])};
+}
+
+void sender::send_bytes(const void *bytes, const size_t len) {
+  ssize_t result = send(socket_fd, bytes, len, 0);
+  if (result == -1) {
+    perror("send() failed");
+    throw std::runtime_error("send() failed");
+  } else if ((size_t)result != len) {
+    fprintf(stderr, "sent %ld bytes, should be %ld bytes!\n", result, len);
+    abort();
+  }
+}
+
+void sender::send_pyobj(const py::object &obj) {
+  py::object dumps = py::module::import("pickle").attr("dumps");
+
+  // serialize obj to binary and get output
+  py::object bytes = dumps(obj, PICKLE_PROTOCOL);
+  PyObject *mem_view = PyMemoryView_GetContiguous(bytes.ptr(), PyBUF_READ, 'C');
+  Py_buffer *buf = PyMemoryView_GET_BUFFER(mem_view);
+
+  // send
+  send_bytes(&(buf->len), sizeof(Py_ssize_t)); // output size
+  send_bytes(buf->buf, buf->len); // output
+}
+
+void sender::dispose() {
+  if (close(socket_fd)) {
+    perror("close() failed");
+    throw std::runtime_error("close() failed");
+  }
+}
+
+void *receiver::receive_bytes(const size_t len) {
+  void *bytes = malloc(len);
+  if (bytes == nullptr) {
+    perror("malloc() failed");
+    throw std::bad_alloc();
+  }
+
+  ssize_t result = recv(socket_fd, bytes, len, 0);
+  if (result == -1) {
+    perror("recv() failed");
+    throw std::runtime_error("recv() failed");
+  } else if ((size_t)result != len) {
+    fprintf(stderr, "received %ld bytes, should be %ld bytes!\n", result, len);
+    abort();
+  }
+
+  return bytes;
+}
+
+py::object receiver::receive_pyobj() {
+  py::object loads = py::module::import("pickle").attr("loads");
+
+  // receive input size
+  Py_ssize_t *size_ptr = reinterpret_cast<Py_ssize_t *>(receive_bytes(sizeof(Py_ssize_t)));
+  Py_ssize_t size = *size_ptr;
+  free(size_ptr);
+
+  // receive input
+  char *bytes = reinterpret_cast<char *>(receive_bytes(size));
+  py::handle mem_view = py::handle(PyMemoryView_FromMemory(bytes, size, PyBUF_READ));
+  return loads(mem_view);
+}
+
+void receiver::dispose() {
+  if (close(socket_fd)) {
+    perror("close() failed");
+    throw std::runtime_error("close() failed");
+  }
+}
+
+} // namespace snakefish
---
+++ src/channel.h
@@ -0,0 +1,191 @@
+#ifndef SNAKEFISH_CHANNEL_H
+#define SNAKEFISH_CHANNEL_H
+
+#include <tuple>
+
+#include <pybind11/pybind11.h>
+namespace py = pybind11;
+
+namespace snakefish {
+
+static const unsigned PICKLE_PROTOCOL = 4;
+
+class receiver; // forward declaration to solve circular dependency
+
+/*
+ * The sending part of an IPC channel.
+ *
+ * Note that the `send_*()` functions do not protect against concurrent accesses.
+ */
+class sender {
+public:
+  /*
+   * No default constructor.
+   */
+  sender() = delete;
+
+  /*
+   * Default destructor that does nothing.
+   *
+   * Since a `sender` is used for IPC, any resources held by it should
+   * only be released by the last involved process. That process should
+   * call the `dispose()` function.
+   */
+  ~sender() = default;
+
+  /*
+   * No copy constructor.
+   */
+  sender(const sender &t) = delete;
+
+  /*
+   * No copy assignment operator.
+   */
+  sender &operator=(const sender &t) = delete;
+
+  /*
+   * Default move constructor.
+   */
+  sender(sender &&t) = default;
+
+  /*
+   * No move assignment operator.
+   */
+  sender &operator=(sender &&t) = delete;
+
+  /*
+   * Create a synchronous channel.
+   *
+   * Here, synchronous means that the sender will block if the channel is full,
+   * and the receiver will block if there's no incoming messages.
+   *
+   * \returns Two pairs of `(sender, receiver)`. One for each communicating
+   * party.
+   */
+  friend std::tuple<sender, receiver, sender, receiver> sync_channel();
+
+  /*
+   * Send some bytes.
+   *
+   * \param bytes Pointer to the start of the bytes.
+   * \param len Number of bytes to send.
+   */
+  void send_bytes(const void *bytes, size_t len);
+
+  /*
+   * Send a python object.
+   *
+   * This function will serialize `obj` using `pickle` and send the binary
+   * output.
+   */
+  void send_pyobj(const py::object &obj);
+
+  /*
+   * Release resources held by this `sender`.
+   */
+  void dispose();
+
+private:
+  /*
+   * Private constructor to be used by the friend functions.
+   */
+  explicit sender(int socket_fd) : socket_fd(socket_fd) {}
+
+  int socket_fd;
+};
+
+/*
+ * The receiving part of an IPC channel.
+ *
+ * Note that the `receive_*()` functions do not protect against concurrent
+ * accesses.
+ */
+class receiver {
+public:
+  /*
+   * No default constructor.
+   */
+  receiver() = delete;
+
+  /*
+   * Default destructor that does nothing.
+   *
+   * Since a `receiver` is used for IPC, any resources held by it should
+   * only be released by the last involved process. That process should
+   * call the `dispose()` function.
+   */
+  ~receiver() = default;
+
+  /*
+   * No copy constructor.
+   */
+  receiver(const receiver &t) = delete;
+
+  /*
+   * No copy assignment operator.
+   */
+  receiver &operator=(const receiver &t) = delete;
+
+  /*
+   * Default move constructor.
+   */
+  receiver(receiver &&t) = default;
+
+  /*
+   * No move assignment operator.
+   */
+  receiver &operator=(receiver &&t) = delete;
+
+  /*
+   * Create a synchronous channel.
+   *
+   * Here, synchronous means that the sender will block if the channel is full,
+   * and the receiver will block if there's no incoming messages.
+   *
+   * \returns Two pairs of `(sender, receiver)`. One for each communicating
+   * party.
+   */
+  friend std::tuple<sender, receiver, sender, receiver> sync_channel();
+
+  /*
+   * Receive some bytes.
+   *
+   * \param len Number of bytes to receive.
+   */
+  void *receive_bytes(size_t len);
+
+  /*
+   * Receive a python object.
+   *
+   * This function will receive some bytes and deserialize them using `pickle`.
+   */
+  py::object receive_pyobj();
+
+  /*
+   * Release resources held by this `receiver`.
+   */
+  void dispose();
+
+private:
+  /*
+   * Private constructor to be used by the friend functions.
+   */
+  explicit receiver(int socket_fd) : socket_fd(socket_fd) {}
+
+  int socket_fd;
+};
+
+/*
+ * Create a synchronous channel.
+ *
+ * Here, synchronous means that the sender will block if the channel is full,
+ * and the receiver will block if there's no incoming messages.
+ *
+ * \returns Two pairs of `(sender, receiver)`. One for each communicating
+ * party.
+ */
+std::tuple<sender, receiver, sender, receiver> sync_channel();
+
+} // namespace snakefish
+
+#endif // SNAKEFISH_CHANNEL_H
